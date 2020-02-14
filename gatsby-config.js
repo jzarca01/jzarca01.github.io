26ee525ab2077c801e2f63f97d31e35fe73a3a9b
Jérémie Zarca <jeremie.zarca@gmail.com>
2020-02-14 17:03:01 +0800

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,6 +68,13 @@ module.exports = {
         path: path.resolve(__dirname, `src/pages`),
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-111460280-1",
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
